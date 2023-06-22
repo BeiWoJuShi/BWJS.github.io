6e95448793df040aae430f2092861186d06fa258
BeiWoJuShi <137387320+BeiWoJuShi@users.noreply.github.com>
2023-06-22 16:10:18 +0800

    Add files via upload

---
+++ Boom.js
@@ -0,0 +1,19 @@
+console.log('Boom.js in');
+
+var image = document.getElementById("myImage");
+
+function start() {
+    image.src = "Õ¨µ¯.gif";
+    }
+
+		
+
+var music = document.getElementById("music");
+
+function playMusic() {
+	if (music.paused) {
+				music.play();
+	} else {
+				music.pause();
+	}
+}
